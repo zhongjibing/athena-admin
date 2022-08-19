4e59ddb2ededbc79626d03fd0ea82c186ef47856
zhongjibing <zhongjibing@163.com>
2022-08-19 17:59:25 +0800

    modify

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,5 @@
-import Cookies from 'js-cookie'
+import store from '@/store'
 
-const TokenKey = 'Admin-Token'
-
-export function getToken() {
+export function checkLogin() {
   return Cookies.get(TokenKey)
-}
-
-export function setToken(token) {
-  return Cookies.set(TokenKey, token)
-}
-
-export function removeToken() {
-  return Cookies.remove(TokenKey)
 }
