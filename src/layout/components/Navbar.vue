77f57f3cbabdb80edde94aae901cfe12a624440e
zhongjibing <zhongjibing@163.com>
2022-08-25 02:48:14 +0800

    modify

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -89,7 +89,7 @@ function logout() {
     type: 'warning'
   }).then(() => {
     userStore.logOut().then(() => {
-      location.href = '/index';
+      location.href = import.meta.env.VITE_APP_LOGIN_PAGE
     })
   }).catch(() => { });
 }
