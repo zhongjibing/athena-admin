deed17daaa23217a15e26fa2eabea7f244208641
zhongjibing <zhongjibing@163.com>
2022-10-21 13:31:21 +0800

    format file

--- src/components/Crontab/year.vue
+++ src/components/Crontab/year.vue
@@ -1,43 +1,43 @@
 <template>
-	<el-form size="small">
-		<el-form-item>
-			<el-radio :label="1" v-model='radioValue'>
-				不填，允许的通配符[, - * /]
-			</el-radio>
-		</el-form-item>
+    <el-form size="small">
+        <el-form-item>
+            <el-radio :label="1" v-model='radioValue'>
+                不填，允许的通配符[, - * /]
+            </el-radio>
+        </el-form-item>
 
-		<el-form-item>
-			<el-radio :label="2" v-model='radioValue'>
-				每年
-			</el-radio>
-		</el-form-item>
+        <el-form-item>
+            <el-radio :label="2" v-model='radioValue'>
+                每年
+            </el-radio>
+        </el-form-item>
 
-		<el-form-item>
-			<el-radio :label="3" v-model='radioValue'>
-				周期从
-				<el-input-number v-model='cycle01' :min='fullYear' :max="maxFullYear - 1" /> -
-				<el-input-number v-model='cycle02' :min="cycle01 + 1" :max="maxFullYear" />
-			</el-radio>
-		</el-form-item>
+        <el-form-item>
+            <el-radio :label="3" v-model='radioValue'>
+                周期从
+                <el-input-number v-model='cycle01' :min='fullYear' :max="maxFullYear - 1" /> -
+                <el-input-number v-model='cycle02' :min="cycle01 + 1" :max="maxFullYear" />
+            </el-radio>
+        </el-form-item>
 
-		<el-form-item>
-			<el-radio :label="4" v-model='radioValue'>
-				从
-				<el-input-number v-model='average01' :min='fullYear' :max="maxFullYear - 1"/> 年开始，每
-				<el-input-number v-model='average02' :min="1" :max="10" /> 年执行一次
-			</el-radio>
+        <el-form-item>
+            <el-radio :label="4" v-model='radioValue'>
+                从
+                <el-input-number v-model='average01' :min='fullYear' :max="maxFullYear - 1"/> 年开始，每
+                <el-input-number v-model='average02' :min="1" :max="10" /> 年执行一次
+            </el-radio>
 
-		</el-form-item>
+        </el-form-item>
 
-		<el-form-item>
-			<el-radio :label="5" v-model='radioValue'>
-				指定
-				<el-select clearable v-model="checkboxList" placeholder="可多选" multiple :multiple-limit="8">
-					<el-option v-for="item in 9" :key="item" :value="item - 1 + fullYear" :label="item -1 + fullYear" />
-				</el-select>
-			</el-radio>
-		</el-form-item>
-	</el-form>
+        <el-form-item>
+            <el-radio :label="5" v-model='radioValue'>
+                指定
+                <el-select clearable v-model="checkboxList" placeholder="可多选" multiple :multiple-limit="8">
+                    <el-option v-for="item in 9" :key="item" :value="item - 1 + fullYear" :label="item -1 + fullYear" />
+                </el-select>
+            </el-radio>
+        </el-form-item>
+    </el-form>
 </template>
 
 <script setup>
