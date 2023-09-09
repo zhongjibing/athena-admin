36e1e58aeb9bb1887209d6d19e8a748a4bb17182
zhongjibing <zhongjibing@163.com>
2023-09-09 23:58:41 +0800

    add type filter

--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -5,12 +5,23 @@
                 <el-input
                     v-model="queryParams.name"
                     placeholder="请输入菜单名称"
+                    style="width: 240px"
                     clearable
                     @keyup.enter="handleQuery"
                 />
             </el-form-item>
+            <el-form-item label="菜单类型" prop="types">
+                <el-select v-model="queryParams.types" placeholder="菜单类型" style="width: 240px" multiple clearable>
+                    <el-option
+                        v-for="type in Object.keys(menuType)"
+                        :key="type"
+                        :label="menuType[type]['name']"
+                        :value="type"
+                    />
+                </el-select>
+            </el-form-item>
             <el-form-item label="状态" prop="status">
-                <el-select v-model="queryParams.status" placeholder="菜单状态" clearable>
+                <el-select v-model="queryParams.status" placeholder="菜单状态" style="width: 240px" clearable>
                     <el-option
                         v-for="dict in sys_normal_disable"
                         :key="dict.value"
@@ -140,15 +151,14 @@
                     <el-col :span="24" v-if="form.type !== 'F'">
                         <el-form-item label="菜单图标" prop="icon">
                             <el-popover
+                                ref="popoverRef"
                                 placement="bottom-start"
                                 :width="540"
-                                v-model:visible="showChooseIcon"
                                 trigger="click"
                                 @show="showSelectIcon"
                             >
                                 <template #reference>
-                                    <el-input v-model="form.icon" placeholder="点击选择图标" @blur="showSelectIcon"
-                                              v-click-outside="hideSelectIcon" readonly>
+                                    <el-input v-model="form.icon" placeholder="点击选择图标" readonly>
                                         <template #prefix>
                                             <svg-icon
                                                 v-if="form.icon"
@@ -157,7 +167,7 @@
                                                 style="height: 32px;width: 16px;"
                                             />
                                             <el-icon v-else style="height: 32px;width: 16px;">
-                                                <search/>
+                                                <Search />
                                             </el-icon>
                                         </template>
                                     </el-input>
@@ -173,7 +183,7 @@
                     </el-col>
                     <el-col :span="12">
                         <el-form-item label="显示排序" prop="orderNum">
-                            <el-input-number v-model="form.orderNum" controls-position="right" :min="0"/>
+                            <el-input-number v-model="form.orderNum" controls-position="right" style="width: auto" :min="0"/>
                         </el-form-item>
                     </el-col>
                     <el-col :span="12" v-if="form.type !== 'F'">
@@ -181,12 +191,13 @@
                             <template #label>
                         <span>
                            <el-tooltip content="选择是外链则路由地址需要以`http(s)://`开头" placement="top">
-                              <el-icon><question-filled/></el-icon>
-                           </el-tooltip>是否外链
+                               <el-icon><QuestionFilled /></el-icon>
+                           </el-tooltip>
+                            是否外链
                         </span>
                             </template>
                             <el-radio-group v-model="form.isFrame">
-                                <el-radio :label="0">否</el-radio>
+                                <el-radio :label="0" style="width: 50px">否</el-radio>
                                 <el-radio :label="1">是</el-radio>
                             </el-radio-group>
                         </el-form-item>
@@ -197,7 +208,7 @@
                         <span>
                            <el-tooltip content="访问的路由地址，如：`user`，如外网地址需内链访问则以`http(s)://`开头"
                                        placement="top">
-                              <el-icon><question-filled/></el-icon>
+                              <el-icon><QuestionFilled /></el-icon>
                            </el-tooltip>
                            路由地址
                         </span>
@@ -211,7 +222,7 @@
                         <span>
                            <el-tooltip content="访问的组件路径，如：`system/user/index`，默认在`views`目录下"
                                        placement="top">
-                              <el-icon><question-filled/></el-icon>
+                              <el-icon><QuestionFilled /></el-icon>
                            </el-tooltip>
                            组件路径
                         </span>
@@ -227,7 +238,7 @@
                            <el-tooltip
                                content="控制器中定义的权限字符，如：@PreAuthorize(`@ss.hasPermi('system:user:list')`)"
                                placement="top">
-                              <el-icon><question-filled/></el-icon>
+                              <el-icon><QuestionFilled /></el-icon>
                            </el-tooltip>
                            权限字符
                         </span>
@@ -240,7 +251,7 @@
                             <template #label>
                         <span>
                            <el-tooltip content='访问路由的默认传递参数，如：`{"id": 1, "name": "xx"}`' placement="top">
-                              <el-icon><question-filled/></el-icon>
+                              <el-icon><QuestionFilled /></el-icon>
                            </el-tooltip>
                            路由参数
                         </span>
@@ -253,7 +264,7 @@
                         <span>
                            <el-tooltip content="选择是则会被`keep-alive`缓存，需要匹配组件的`name`和地址保持一致"
                                        placement="top">
-                              <el-icon><question-filled/></el-icon>
+                              <el-icon><QuestionFilled /></el-icon>
                            </el-tooltip>
                            是否缓存
                         </span>
@@ -269,7 +280,7 @@
                             <template #label>
                         <span>
                            <el-tooltip content="选择隐藏则路由将不会出现在侧边栏，但仍然可以访问" placement="top">
-                              <el-icon><question-filled/></el-icon>
+                              <el-icon><QuestionFilled /></el-icon>
                            </el-tooltip>
                            显示状态
                         </span>
@@ -289,7 +300,7 @@
                             <template #label>
                         <span>
                            <el-tooltip content="选择停用则路由将不会出现在侧边栏，也不能被访问" placement="top">
-                              <el-icon><question-filled/></el-icon>
+                              <el-icon><QuestionFilled /></el-icon>
                            </el-tooltip>
                            菜单状态
                         </span>
@@ -320,7 +331,6 @@
 import { addMenu, delMenu, getMenu, listMenu, updateMenu } from "@/api/system/menu"
 import SvgIcon from "@/components/SvgIcon"
 import IconSelect from "@/components/IconSelect"
-import { ClickOutside as vClickOutside } from 'element-plus'
 
 const { proxy } = getCurrentInstance()
 const { sys_show_hide, sys_normal_disable } = proxy.useDict("sys_show_hide", "sys_normal_disable")
@@ -333,14 +343,15 @@ const title = ref("")
 const menuOptions = ref([])
 const isExpandAll = ref(false)
 const refreshTable = ref(true)
-const showChooseIcon = ref(false)
 const iconSelectRef = ref(null)
+const popoverRef = ref(null)
 
 const data = reactive({
     form: {},
     queryParams: {
         name: undefined,
-        visible: undefined
+        status: undefined,
+        types: undefined
     },
     rules: {
         name: [{required: true, message: "菜单名称不能为空", trigger: "blur"}],
@@ -402,22 +413,12 @@ function reset() {
 /** 展示下拉图标 */
 function showSelectIcon() {
     iconSelectRef.value.reset()
-    showChooseIcon.value = true
 }
 
 /** 选择图标 */
 function selected(name) {
     form.value.icon = name
-    showChooseIcon.value = false
-}
-
-/** 图标外层点击隐藏下拉列表 */
-function hideSelectIcon(event) {
-    var elem = event.relatedTarget || event.srcElement || event.target || event.currentTarget
-    var className = elem.className
-    if (className !== "el-input__inner") {
-        showChooseIcon.value = false
-    }
+    popoverRef.value.hide()
 }
 
 /** 搜索按钮操作 */
