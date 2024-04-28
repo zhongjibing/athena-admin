d2c6c119c4e4d75a3060329f824c7a309916691a
zhongjibing <zhongjibing@163.com>
2024-04-28 18:32:56 +0800

    modify conf files

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite/client" />
-declare module '*.vue' {
-    import { DefineComponent } from 'vue'
-    const component: DefineComponent<{}, {}, any>
-    export default component
-}
-
-declare module 'crypto-js'
-declare module 'sm-crypto'
-
-declare module '@/types/global'
